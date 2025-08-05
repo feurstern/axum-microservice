b134634eaffaa75382af491139f03c567deb33f0
feurstern <naziincolour123@gmail.com>
2025-08-05 16:11:58 +0700

    #5 added the validatio nfor the email and the exising user

--- src/services/user_service.rs
+++ src/services/user_service.rs
@@ -1,4 +1,7 @@
-use crate::models::user::{NewUser, User};
+use crate::{
+    db::database::DbPool,
+    models::user::{NewUser, User},
+};
 use axum::{Json, extract::State, http::StatusCode};
 use serde_json::json;
 use sqlx::{Pool, Postgres};
@@ -7,9 +10,27 @@ pub async fn create_user(
     State(pool): State<Pool<Postgres>>,
     Json(new_user): Json<NewUser>,
 ) -> Result<Json<User>, (StatusCode, Json<serde_json::Value>)> {
+    if !new_user.email.contains('@') {
+        return Err((
+            StatusCode::BAD_REQUEST,
+            Json(json!({"error":"Invalid email address" })),
+        ));
+    }
 
+    let existing_user = sqlx::query!(r#"SELECT id from users where email=$1"#, new_user.email)
+        .fetch_optional(&pool)
+        .await
+        .map_err(|e| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"error"  : format!("Something went wrong :" ,e)})),
+            )
+        })?;
 
-    // let existing_email = sqlx
+    if existing_user.is_some(){
+        return Err((StatusCode::BAD_REQUEST, Json(json!("Email is already taken!"))));
+    }
+
     let user = sqlx::query_as!(
         User,
         r#"
@@ -36,3 +57,5 @@ pub async fn create_user(
 
     Ok(Json(user))
 }
+
+pub async fn delete_user(State(pool): State<Pool<Postgres>>, user_id: i32) {}
